7ae099be3fbeae5382c801448dec73f5deb33406
shenliangliang <shenliangliang@gamehero.me>
2017-02-07 19:10:22 +0800

    *初始化

---
+++ demo1/Assets/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	private int a;
+	void Start () {
+		a = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void addCount(){
+		
+	}
+}
